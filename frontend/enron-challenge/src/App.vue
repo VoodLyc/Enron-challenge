a91546c450d38d027a9f180eae0d5d718b876507
Johan Sebastián Giraldo Rubio <j.sebasgr036@gmail.com>
2024-02-15 01:58:17 -0500

    Add search functionality

--- frontend/enron-challenge/src/App.vue
+++ frontend/enron-challenge/src/App.vue
@@ -1,14 +1,23 @@
 <template>
-  <DataTable :value="emails" showGridlines stripedRows :size="small" lazy paginator :first="first" :rows="10"
-    :totalRecords="totalRecords" :loading="loading" @page="onPage($event)" :rowsPerPageOptions="[5, 10, 20]"
-    paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
-    currentPageReportTemplate="{first} to {last} of {totalRecords}">
-    <Column field="message_id" header="Id"></Column>
-    <Column field="subject" header="Subject"></Column>
-    <Column field="from" header="From"></Column>
-    <Column field="to" header="To"></Column>
-    <Column field="content" header="Content"></Column>
-  </DataTable>
+  <div>
+    <div class="ml-2 mb-4 mt-2 flex flex-row items-center">
+      <InputGroup class="w-1/4">
+        <InputText placeholder="Keyword" v-model="searchTerm" />
+        <Button icon="pi pi-search" @click="onSearch()" />
+      </InputGroup>
+      <Button icon="pi pi-times" class="ml-3" v-if="filtered" severity="danger" rounded aria-label="Cancel" @click="unfilterData()" />
+    </div>
+    <DataTable :value="emails" showGridlines stripedRows lazy paginator :first="first" :rows="10"
+      :totalRecords="totalRecords" :loading="loading" @page="onPage($event)" :rowsPerPageOptions="[5, 10, 20]"
+      paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
+      currentPageReportTemplate="{first} to {last} of {totalRecords}">
+      <Column field="message_id" header="Id"></Column>
+      <Column field="subject" header="Subject"></Column>
+      <Column field="from" header="From"></Column>
+      <Column field="to" header="To"></Column>
+      <Column field="content" header="Content"></Column>
+    </DataTable>
+  </div>
 </template>
 
 <script setup>
@@ -20,16 +29,28 @@ const totalRecords = ref(0);
 const loading = ref(false);
 const lazyParams = ref({});
 const emails = ref([]);
+const searchTerm = ref("");
+const filtered = ref(false);
 
 const loadLazyData = (event) => {
   loading.value = true;
-  console.log(event);
   lazyParams.value = { ...lazyParams.value, first: event?.first || first.value };
-  EmailService.getEmails(lazyParams.value.first).then((data) => {
-    emails.value = data.emails;
-    totalRecords.value = data.total;
-    loading.value = false;
-  });
+  if (filtered.value) {
+    EmailService.searchEmails(searchTerm.value, lazyParams.value.first)
+      .then((data) => {
+        emails.value = data.emails;
+        totalRecords.value = data.total;
+        loading.value = false;
+      })
+  }
+  else {
+    EmailService.getEmails(lazyParams.value.first)
+      .then((data) => {
+        emails.value = data.emails;
+        totalRecords.value = data.total;
+        loading.value = false;
+      });
+  }
 }
 
 const onPage = (event) => {
@@ -37,6 +58,24 @@ const onPage = (event) => {
   loadLazyData(event);
 }
 
+const onSearch = () => {
+  lazyParams.value = {
+    first: 0,
+    rows: 10
+  }
+  filtered.value = true;
+  loadLazyData();
+
+}
+
+const unfilterData = () => {
+  filtered.value = false;
+  lazyParams.value = {
+    first: 0,
+    rows: 10
+  };
+  loadLazyData();
+}
 
 onMounted(() => {
   lazyParams.value = {
@@ -47,5 +86,3 @@ onMounted(() => {
 });
 
 </script>
-
-<style></style>
--- frontend/enron-challenge/src/main.js
+++ frontend/enron-challenge/src/main.js
@@ -5,7 +5,10 @@ import PrimeVue from 'primevue/config';
 import Wind from './presets/wind';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
-
+import InputGroup from 'primevue/inputgroup';
+import InputGroupAddon from 'primevue/inputgroupaddon';
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
 
 const app = createApp(App);
 app.use(PrimeVue, {
@@ -14,4 +17,8 @@ app.use(PrimeVue, {
 });
 app.component('DataTable', DataTable);
 app.component('Column', Column);
+app.component('InputGroup', InputGroup);
+app.component('InputGroupAddon', InputGroupAddon);
+app.component('Button', Button);
+app.component('InputText', InputText);
 app.mount('#app');
